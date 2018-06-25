bda3afd2fd99d35bed437e097ebb5e765cf70a8f
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-06-25 19:28:11 +0200

    Upgrade to 3.4 & Chapter 6: Authenticator: getUser, checkCredentials & Success/Failure

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -16,15 +16,15 @@ class SecurityController extends Controller
 	{
 		$authenticationUtils = $this->get('security.authentication_utils');
 
-	    // get the login error if there is one
-	    $error = $authenticationUtils->getLastAuthenticationError();
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
 
-	    // last username entered by the user
-	    $lastUsername = $authenticationUtils->getLastUsername();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-	    $form = $this->createForm(LoginForm::class, [ 
-			'_username' => $lastUsername,
-		]);
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername,
+        ]);
 
 	    return $this->render('security/login.html.twig', array(
 	        'form' => $form->createView(),
--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -2,23 +2,30 @@
 
 namespace AppBundle\Security;
 
+use AppBundle\Form\LoginForm;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Routing\RouterInterface;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
+	private $formFactory;
+	private $em;
+	private $router;
+
+	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+	{
+		$this->formFactory = $formFactory;
+		$this->em = $em;
+		$this->router = $router;
+	}
+
 	public function getCredentials(Request $request)
 	{
-		private $formFactory;
-
-		public function __construct(FormFactoryInterface $formFactory)
-		{
-			$this->formFactory = $formFactory;
-		}
-
 		$isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
 
 		if (!$isLoginSubmit) {
@@ -26,8 +33,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 		}
 
 		$form = $this->formFactory->create(LoginForm::class);
-
-		$form->handleRequest($request);
+        $form->handleRequest($request);
 
 		$data = $form->getData();
 
@@ -37,19 +43,28 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
 	public function getUser($credentials, UserProviderInterface $userProvider)
 	{
+		$username = $credentials['_username'];
 
+		return $this->em->getRepository('AppBundle:User') 
+			->findOneBy(['email' => $username]);
 	}
 
 	public function checkCredentials($credentials, UserInterface $user)
 	{
-
+		$password = $credentials['_password'];
+		if ($password == 'iliketurtles') { 
+			return true;
+		}
+		return false;
 	}
 
 	protected function getLoginUrl() 
 	{
+		return $this->router->generate('security_login');
 	}
 
 	protected function getDefaultSuccessRedirectUrl() 
 	{
+		return $this->router->generate('homepage');
 	}
 }
