ee25966acf3b01db5f764b1bd6c2cf01562c21ce
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-06-25 11:16:28 +0200

    Chapter 2: The UserInterface Methods

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,15 +2,39 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Role\Role;
  
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user") 
+ * 
+ */
 class User implements UserInterface
 {
+	/**
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO") 
+	 * @ORM\Column(type="integer")
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="string", unique=true)
+	 */
+	private $email;
+
 	public function getUsername() {
+
+		return $this->email;
+
 	}
 
 	public function getRoles() {
+
+		return ['ROLE_USER'];
+
 	}
 
 	public function getPassword() {
@@ -18,8 +42,12 @@ class User implements UserInterface
 
 	public function getSalt() {
 	}
-	
+
 	public function eraseCredentials() {
+	}
+
+	public function setEmail($email) {
+		$this->email = $email; 
 	}
 
 }
