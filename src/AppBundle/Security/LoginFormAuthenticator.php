78381a0475070bcf8043b8cd765832606627616b
VirginieGodfrin <virginiegodfrin@gmail.com>
2018-06-26 11:40:59 +0200

    Chapter 11: Configuring the Encoder in security.yml

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -11,18 +11,21 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
 	private $formFactory;
 	private $em;
 	private $router;
+	private $passwordEncoder;
 
-	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+	public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router, UserPasswordEncoder $passwordEncoder)
 	{
 		$this->formFactory = $formFactory;
 		$this->em = $em;
 		$this->router = $router;
+		$this->passwordEncoder = $passwordEncoder;
 	}
 
 	public function getCredentials(Request $request)
@@ -58,10 +61,12 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 	public function checkCredentials($credentials, UserInterface $user)
 	{
 		$password = $credentials['_password'];
-		if ($password == 'iliketurtles') { 
-			return true;
-		}
-		return false;
+		
+		if ($this->passwordEncoder->isPasswordValid($user, $password)) {
+            return true;
+        }
+
+        return false;
 	}
 
 	protected function getLoginUrl() 
